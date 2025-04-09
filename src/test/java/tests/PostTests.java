2c026673141ade3623ebaf0f012a505da59d39d1
Gnanaprasad R <gnanaprasad.r@dxc.com~>
2025-04-09 12:51:50 +0530

    Modified post test class to align with generating extend report

--- src/test/java/tests/PostTests.java
+++ src/test/java/tests/PostTests.java
@@ -5,25 +5,61 @@ import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 import org.testng.annotations.Test;
 
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
 public class PostTests extends BaseTest {
 
     @Test
     public void createPost() {
+        getTest().info("Starting test: createPost");
+
         String requestBody = "{\n" +
                 "  \"title\": \"foo\",\n" +
                 "  \"body\": \"bar\",\n" +
                 "  \"userId\": 1\n" +
                 "}";
 
-        given()
-            .baseUri("https://jsonplaceholder.typicode.com")
-            .header("Content-type", "application/json")
-            .body(requestBody)
-        .when()
-            .post("/posts")
-        .then()
-            .statusCode(201)
-            .body("id", notNullValue())
-            .body("title", equalTo("foo"));
+        getTest().info("Request Body:\n" + requestBody);
+
+        Response response = given()
+                .baseUri("https://jsonplaceholder.typicode.com")
+                .header("Content-type", "application/json")
+                .log().all() // logs request
+                .body(requestBody)
+                .when()
+                .post("/posts");
+
+        getTest().info("Sent POST request to /posts");
+
+        int statusCode = response.getStatusCode();
+        getTest().info("Received status code: " + statusCode);
+
+        String responseBody = response.getBody().asPrettyString();
+        getTest().info("Response Body:\n" + responseBody);
+
+        if (statusCode == 201) {
+            getTest().pass("Status code is 201 - Created");
+        } else {
+            getTest().fail("Expected status code 201 but got: " + statusCode);
+        }
+
+        Integer id = response.jsonPath().getInt("id");
+        String title = response.jsonPath().getString("title");
+
+        if (id != null) {
+            getTest().pass("Post ID is present: " + id);
+        } else {
+            getTest().fail("Post ID is null or missing");
+        }
+
+        if ("foo".equals(title)) {
+            getTest().pass("Title matches expected value 'foo'");
+        } else {
+            getTest().fail("Title mismatch. Expected 'foo', got: " + title);
+        }
     }
 }
